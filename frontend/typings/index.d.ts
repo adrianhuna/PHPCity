f9c4f5a55a37757eeb93913fc8b3a870a8181ebb
Adrian Huna <huna.adrian@gmail.com>
2016-09-15 12:26:37 +0200

    Release

--- frontend/typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/bootstrap/index.d.ts" />
-/// <reference path="globals/d3/index.d.ts" />
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/stats/index.d.ts" />
-/// <reference path="globals/three-orbitcontrols/index.d.ts" />
-/// <reference path="globals/three/index.d.ts" />
-/// <reference path="globals/underscore/index.d.ts" />
